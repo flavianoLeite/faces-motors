d40dd7552dfc7c6c04e6677a02bffda086ed2168
Flaviano Linux Lenovo <flaviano_leite@yahoo.com.br>
2016-03-15 22:58:49 -0300

    Treinando jsf

---
+++ src/utils/AutenticacaoPhaseListener.java
@@ -0,0 +1,32 @@
+package utils;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+public class AutenticacaoPhaseListener implements PhaseListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6577990585903051645L;
+
+	@Override
+	public void afterPhase(PhaseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforePhase(PhaseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public PhaseId getPhaseId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
